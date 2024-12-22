118116ce620a324fd2d6f66b581eb9754c9eb86e
Thomas Epperson <thomas.epperson@gmail.com>
2024-12-22 10:41:03 -0600

    Write a uart register as a simple test.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
 
+struct UartRegisters {
+    regs: [u32; 16],
+}
+
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     loop {
@@ -9,5 +13,8 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let mut uart : &mut UartRegisters = unsafe { &mut *(0x00030000 as *mut UartRegisters) };
+    uart.regs[0] = 42;
+
     loop {}
 }
